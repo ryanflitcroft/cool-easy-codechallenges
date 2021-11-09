fa0f43b5531cb201f3f39e816c2e3fc3a9b35500
sarah-svg <kjskillen@gmail.com>
2021-11-08 19:31:09 -0800

    had to fix the imports

--- test/tests.js
+++ test/tests.js
@@ -3,9 +3,11 @@
 import './example.test.js';
  //// Import add tests here.
 import '../add-function/add.test.js';
-/// Import subtract tests here.
+// /// Import subtract tests here.
 import '../division-function/division.test.js';
-/// Import subtraction tests here.
-import '../subtraction-function/subtraction.test.js';
-//// Import multiply tests here.
-import '../multiplication-function/multiplication.test.js';
+// /// Import subtraction tests here.
+import '../subtract-function/subtract.test.js';
+// //// Import multiply tests here.
+import '../multiply-function/multiply.test.js';
+// /// Import string tests here.
+import '../strings/strings.test.js';
