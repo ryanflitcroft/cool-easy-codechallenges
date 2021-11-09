8d28baac0d59031f2050386f5cc0ee39506e78c4
sarah-svg <kjskillen@gmail.com>
2021-11-08 20:09:47 -0800

    tests need to pass

--- arrays/array.js
+++ arrays/array.js
@@ -1,9 +1,10 @@
 /// directions in markdown
 
 
-
-export function addOneToEndOfArray(array, newEntry) {
+                                    
+export function addOneToEndOfArray() {
     /// the first function is here as a example but I want you to finish it using the .push() method.
+    ///it takes in two parameters, the first is the array and the second is the value to add to the end of the array.
   ///this is just there to to pass the test
     return [1, 2, 3, 4, 5];
 }
